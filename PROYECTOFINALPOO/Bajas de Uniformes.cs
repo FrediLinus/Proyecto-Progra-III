c4ab6189a5c720eac606af57e0d2cb9a3a09aeea
FrediLinus <frediorozco076@gmail.com>
2026-05-31 23:31:02 -0600

    creacion formularios fredi

---
+++ PROYECTOFINALPOO/Bajas de Uniformes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROYECTOFINALPOO
+{
+    public partial class Bajas_de_Uniformes : Form
+    {
+        public Bajas_de_Uniformes()
+        {
+            InitializeComponent();
+        }
+    }
+}
