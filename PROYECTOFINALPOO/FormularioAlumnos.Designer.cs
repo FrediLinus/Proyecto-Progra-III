47853858e560bac607d609d91486cb97a17674c8
SamuelNavarro1441 <samuelnavarro1441@gmail.com>
2026-06-01 00:21:59 -0600

    vincular

---
+++ PROYECTOFINALPOO/FormularioAlumnos.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PROYECTOFINALPOO
+{
+    partial class FormularioAlumnos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAlumnos = new System.Windows.Forms.Button();
+            this.btnAsistencia = new System.Windows.Forms.Button();
+            this.btnNotas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAlumnos
+            // 
+            this.btnAlumnos.Location = new System.Drawing.Point(130, 61);
+            this.btnAlumnos.Name = "btnAlumnos";
+            this.btnAlumnos.Size = new System.Drawing.Size(137, 75);
+            this.btnAlumnos.TabIndex = 0;
+            this.btnAlumnos.Text = "Alumnos";
+            this.btnAlumnos.UseVisualStyleBackColor = true;
+            this.btnAlumnos.Click += new System.EventHandler(this.btnAlumnos_Click);
+            // 
+            // btnAsistencia
+            // 
+            this.btnAsistencia.Location = new System.Drawing.Point(309, 62);
+            this.btnAsistencia.Name = "btnAsistencia";
+            this.btnAsistencia.Size = new System.Drawing.Size(139, 74);
+            this.btnAsistencia.TabIndex = 1;
+            this.btnAsistencia.Text = "Asistencia";
+            this.btnAsistencia.UseVisualStyleBackColor = true;
+            this.btnAsistencia.Click += new System.EventHandler(this.btnAsistencia_Click);
+            // 
+            // btnNotas
+            // 
+            this.btnNotas.Location = new System.Drawing.Point(502, 62);
+            this.btnNotas.Name = "btnNotas";
+            this.btnNotas.Size = new System.Drawing.Size(139, 74);
+            this.btnNotas.TabIndex = 2;
+            this.btnNotas.Text = "Notas";
+            this.btnNotas.UseVisualStyleBackColor = true;
+            this.btnNotas.Click += new System.EventHandler(this.btnNotas_Click);
+            // 
+            // FormularioAlumnos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNotas);
+            this.Controls.Add(this.btnAsistencia);
+            this.Controls.Add(this.btnAlumnos);
+            this.Name = "FormularioAlumnos";
+            this.Text = "FormularioAlumnos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAlumnos;
+        private System.Windows.Forms.Button btnAsistencia;
+        private System.Windows.Forms.Button btnNotas;
+    }
+}
---
+++ PROYECTOFINALPOO/FormularioAlumnos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROYECTOFINALPOO
+{
+    public partial class FormularioAlumnos : Form
+    {
+        public FormularioAlumnos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAlumnos_Click(object sender, EventArgs e)
+        {
+            Alumons alumnos = new Alumons();
+            alumnos.Show();
+        }
+
+        private void btnAsistencia_Click(object sender, EventArgs e)
+        {
+            Asitencia asis = new Asitencia();
+            asis.Show();
+        }
+
+        private void btnNotas_Click(object sender, EventArgs e)
+        {
+            ControlNotas notas = new ControlNotas();
+            notas.Show();
+        }
+    }
+}
