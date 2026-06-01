bb90bee394df8318078cdd576c8b7ed4374f7eb9
FrediLinus <frediorozco076@gmail.com>
2026-06-01 00:20:27 -0600

    ingreso de formularios

--- PROYECTOFINALPOO/Menu Admin.cs
+++ PROYECTOFINALPOO/Menu Admin.cs
@@ -16,5 +16,41 @@ namespace PROYECTOFINALPOO
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ingreso_de_Uniformes ingreso_De_Uniformes = new Ingreso_de_Uniformes();
+            ingreso_De_Uniformes.Show();
+        }
+
+        private void btnBajas_Click(object sender, EventArgs e)
+        {
+            Bajas_de_Uniformes bajas_De_Uniformes = new Bajas_de_Uniformes();
+            bajas_De_Uniformes.Show();
+        }
+
+        private void btnInventario_Click(object sender, EventArgs e)
+        {
+            Inventario inventario = new Inventario();
+            inventario.Show();
+        }
+
+        private void btnVentas_Click(object sender, EventArgs e)
+        {
+            Ventas ventas = new Ventas();
+            ventas.Show();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            Form1 menu = new Form1();
+            menu.Show();
+            this.Hide();
+        }
     }
 }
