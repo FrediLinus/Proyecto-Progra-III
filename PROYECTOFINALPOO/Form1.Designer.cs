7fcf7ab7a091193ea1e42c2621f8521debe91b1f
SamuelNavarro1441 <samuelnavarro1441@gmail.com>
2026-06-02 22:53:50 -0600

    cambios login

--- PROYECTOFINALPOO/Form1.Designer.cs
+++ PROYECTOFINALPOO/Form1.Designer.cs
@@ -29,35 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.Nombre = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtContra = new System.Windows.Forms.TextBox();
             this.btnIngrear = new System.Windows.Forms.Button();
-            this.btnSalir = new System.Windows.Forms.Button();
             this.chbMirarContra = new System.Windows.Forms.CheckBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(297, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 33);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "LOGIN";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(414, 100);
+            this.label2.Location = new System.Drawing.Point(483, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 1;
@@ -65,27 +53,30 @@
             // txtUsuario
             // 
             this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(243, 150);
+            this.txtUsuario.Location = new System.Drawing.Point(363, 240);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(137, 26);
+            this.txtUsuario.Size = new System.Drawing.Size(159, 26);
             this.txtUsuario.TabIndex = 2;
             // 
             // Nombre
             // 
             this.Nombre.AutoSize = true;
             this.Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Nombre.Location = new System.Drawing.Point(124, 150);
+            this.Nombre.Location = new System.Drawing.Point(241, 243);
+            this.Nombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Nombre.Name = "Nombre";
-            this.Nombre.Size = new System.Drawing.Size(64, 20);
+            this.Nombre.Size = new System.Drawing.Size(71, 20);
             this.Nombre.TabIndex = 3;
-            this.Nombre.Text = "LOGIN";
+            this.Nombre.Text = "Usuario";
             this.Nombre.Click += new System.EventHandler(this.label3_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(112, 212);
+            this.label3.Location = new System.Drawing.Point(224, 291);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(102, 20);
             this.label3.TabIndex = 4;
@@ -94,37 +85,31 @@
             // txtContra
             // 
             this.txtContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContra.Location = new System.Drawing.Point(243, 212);
+            this.txtContra.Location = new System.Drawing.Point(363, 285);
+            this.txtContra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtContra.Name = "txtContra";
-            this.txtContra.Size = new System.Drawing.Size(137, 26);
+            this.txtContra.Size = new System.Drawing.Size(159, 26);
             this.txtContra.TabIndex = 5;
             // 
             // btnIngrear
             // 
+            this.btnIngrear.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.btnIngrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIngrear.Location = new System.Drawing.Point(233, 269);
+            this.btnIngrear.Location = new System.Drawing.Point(363, 369);
+            this.btnIngrear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnIngrear.Name = "btnIngrear";
-            this.btnIngrear.Size = new System.Drawing.Size(181, 51);
+            this.btnIngrear.Size = new System.Drawing.Size(211, 63);
             this.btnIngrear.TabIndex = 6;
             this.btnIngrear.Text = "🔐 Ingresar al Sistema";
-            this.btnIngrear.UseVisualStyleBackColor = true;
+            this.btnIngrear.UseVisualStyleBackColor = false;
             this.btnIngrear.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnSalir
-            // 
-            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(233, 336);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(181, 47);
-            this.btnSalir.TabIndex = 7;
-            this.btnSalir.Text = "🚪 Salir del Sistema";
-            this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // chbMirarContra
             // 
             this.chbMirarContra.AutoSize = true;
             this.chbMirarContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chbMirarContra.Location = new System.Drawing.Point(398, 214);
+            this.chbMirarContra.Location = new System.Drawing.Point(540, 287);
+            this.chbMirarContra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chbMirarContra.Name = "chbMirarContra";
             this.chbMirarContra.Size = new System.Drawing.Size(184, 24);
             this.chbMirarContra.TabIndex = 8;
@@ -138,22 +123,22 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImage = global::PROYECTOFINALPOO.Properties.Resources.Gemini_Generated_Image_3bgit13bgit13bgi;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 554);
             this.Controls.Add(this.chbMirarContra);
-            this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnIngrear);
             this.Controls.Add(this.txtContra);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Nombre);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login Usuarios";
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -161,15 +146,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Label Nombre;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtContra;
         private System.Windows.Forms.Button btnIngrear;
-        private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.CheckBox chbMirarContra;
         private System.Windows.Forms.ErrorProvider errorProvider1;
     }
--- PROYECTOFINALPOO/Properties/Resources.Designer.cs
+++ PROYECTOFINALPOO/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace PROYECTOFINALPOO.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,16 @@ namespace PROYECTOFINALPOO.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Gemini_Generated_Image_3bgit13bgit13bgi {
+            get {
+                object obj = ResourceManager.GetObject("Gemini_Generated_Image_3bgit13bgit13bgi", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
